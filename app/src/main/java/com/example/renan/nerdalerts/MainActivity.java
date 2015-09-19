9f497f3065168c8574fe03260ebe41a0106e26af
Renan Cunha <renan.cunha33@gmail.com>
2015-09-19 18:03:00 -0300

    novos sons e correção de bugs.

--- app/src/main/java/com/example/renan/nerdalerts/MainActivity.java
+++ app/src/main/java/com/example/renan/nerdalerts/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.renan.nerdalerts;
 
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.MediaPlayer;
@@ -10,36 +11,88 @@ import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    MediaPlayer som;
+    boolean mambo=false ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#270e5b")));
+        assert actionBar != null;
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#2cccbc")));
+
 
     }
     public void somNoob(View v){
-        final MediaPlayer som= MediaPlayer.create(this,R.raw.noobalert);
+        valida();
+        som= MediaPlayer.create(this, R.raw.noobalert);
         som.start();
+        mambo = true;
     }
     public void somMentira(View v){
-        final MediaPlayer som= MediaPlayer.create(this,R.raw.mentiraalert);
+        valida();
+        som= MediaPlayer.create(this,R.raw.mentiraalert);
         som.start();
+        mambo = true;
     }
     public void somFunk(View v){
-        final MediaPlayer som= MediaPlayer.create(this,R.raw.funkalert);
+        valida();
+        som= MediaPlayer.create(this,R.raw.funkalert);
         som.start();
+        mambo = true;
     }
     public void somMasc(View v){
-        final MediaPlayer som= MediaPlayer.create(this,R.raw.masculinityalert);
+        valida();
+            som = MediaPlayer.create(this, R.raw.masculinityalert);
+            som.start();
+        mambo = true;
+
+    }
+    public void somFominha(View v){
+        valida();
+        som= MediaPlayer.create(this,R.raw.fominhaalert);
         som.start();
+        mambo = true;
+    }
+    public void somNojo(View v){
+        valida();
+        som= MediaPlayer.create(this,R.raw.nojoalert);
+        som.start();
+        mambo = true;
+    }
+    public void somNovela(View v){
+        valida();
+        som= MediaPlayer.create(this,R.raw.noveleiroalert);
+        som.start();
+        mambo = true;
+    }
+    public void somActor(View v){
+        valida();
+        som= MediaPlayer.create(this,R.raw.overactoralert);
+        som.start();
+        mambo = true;
     }
     public void somTarado(View v){
-        final MediaPlayer som= MediaPlayer.create(this,R.raw.taradoalert);
+        valida();
+       som= MediaPlayer.create(this,R.raw.taradoalert);
         som.start();
+        mambo = true;
     }
+    public void somNerd(View v){
+        valida();
+        som= MediaPlayer.create(this,R.raw.nerdoffice);
+        som.start();
+        mambo = true;
+    }
+public void valida(){
+
+    if (mambo){
+        som.stop();
+        mambo=false;
+    }
+
+}
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -56,9 +109,16 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
+        if (id == R.id.action_settings) {
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setTitle("About...");
+            alert.setMessage("Nerd Alerts\n\nDeveloped by: Renan Cunha \nEmail: renan.cunha33@gmail.com");
+            alert.setNeutralButton("OK", null);
+            alert.show();
+
             return true;
-        }*/
+        }
 
         return super.onOptionsItemSelected(item);
     }
